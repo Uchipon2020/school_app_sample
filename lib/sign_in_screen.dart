ae36eb77a38e71e2d29ce16b51fe86c3beeef1e3
satoshi <uchiyama.s@timeshappy.com>
2022-05-29 10:31:53 +0900

    0529001

--- lib/sign_in_screen.dart
+++ lib/sign_in_screen.dart
@@ -6,55 +6,81 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'School Database App',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'メールアドレス'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(height: 8),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'パスワード'),
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-              ),
-              SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => {},
-                  child: Text('ログイン'),
+      body: Form(
+        //FromのKeyに指定する
+        key: _formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'School Database App',
+                  style: Theme.of(context).textTheme.headline4,
                 ),
-              ),
-              SizedBox(height: 8),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () => {},
-                  child: Text('新規登録'),
+                SizedBox(height: 16),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'メールアドレス'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (String? value) {
+                    if (value?.isEmpty == true) {
+                      return 'Eメールアドレスを入力してください';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-            ],
+                SizedBox(height: 8),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'パスワード'),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  validator: (String? value) {
+                    if (value?.isEmpty == true) {
+                      return 'パスワードを入力してください';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _onSignIn(),
+                    child: Text('ログイン'),
+                  ),
+                ),
+                SizedBox(height: 8),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () => _onSignUp(),
+                    child: Text('新規登録'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _onSignIn(){
+    if(_formKey.currentState?.validate() != true){
+      return;
+    }
+  }
+
+  void _onSignUp(){
+    if(_formKey.currentState?.validate() != true){
+      return;
+    }
   }
 }
