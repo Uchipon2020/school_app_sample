cd9266ffc3848b3711e3463b0bb1bf42d69d2cb4
hu-mania <uchiyama.s@timeshappy.com>
2022-05-30 19:09:58 +0900

    0530003

--- lib/photo_list_screen.dart
+++ lib/photo_list_screen.dart
@@ -6,8 +6,126 @@ class PhotoListScreen extends StatefulWidget {
 }
 
 class _PhotoListScreenState extends State<PhotoListScreen> {
+  late int _currentIndex;
+  late PageController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = 0;
+    _controller = PageController(initialPage: _currentIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student Data'),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _controller,
+        onPageChanged: (int index) => _onPageChanged(index),
+        children: [
+          PhotoGridView(),
+          PhotoGridView(),
+          /*Container(
+            child: Center(
+              child: Text('フォト'),
+            ),
+          ),
+          Container(
+            child: Center(
+              child: Text('お気に入り'),
+            ),
+          ),*/
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) => _onTapBottomNavigaionItem(index),
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image),
+            label: 'フォト',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'お気に入り',
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  void _onTapBottomNavigaionItem(int index) {
+    _controller.animateToPage(
+      index,
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+    );
+
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+class PhotoGridView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> imageList = [
+      'https://placehold.jp/400x300.png?text=0',
+      'https://placehold.jp/400x300.png?text=0',
+      'https://placehold.jp/400x300.png?text=0',
+      'https://placehold.jp/400x300.png?text=0',
+      'https://placehold.jp/400x300.png?text=0',
+      'https://placehold.jp/400x300.png?text=0',
+    ];
+    return GridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+      padding: const EdgeInsets.all(8),
+      children: imageList.map((String imageURL) {
+        return Stack(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: InkWell(
+                onTap: () => {},
+                child: Image.network(
+                  imageURL,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () => {},
+                color: Colors.white,
+                icon: Icon(Icons.favorite_border),
+              ),
+            ),
+          ],
+        );
+      }).toList(),
+    );
   }
 }
