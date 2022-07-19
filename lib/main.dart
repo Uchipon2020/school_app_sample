24ba9b6dc78c4d9f490eb04bb2abb6d7406e316f
hu-mania <uchiyama.s@timeshappy.com>
2022-07-19 10:31:57 +0900

    Healthcare mania include task4　Step2

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'sign_in_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: SignInScreen(),
+      home: const SignInScreen(),
     );
   }
 }
